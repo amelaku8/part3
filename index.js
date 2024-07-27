0c633ebb9a1c2375d9112f571138f0ad4309990b
amelaku8 <amelaku8@gmail.com>
2024-07-27 16:02:28 +0300

    added frontend

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const app =express()
 
 morgan.token('tpe', function (req, res) { return JSON.stringify(req.body) })
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :tpe'))
 
